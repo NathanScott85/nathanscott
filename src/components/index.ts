e9241973019e4deadeb5db8c0e019bc320c9ca62
NathanScott85 <contact@nathanscott.net>
2021-01-23 16:17:09 +0000

    added svgs

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import Development from './img/svg/development';
+import Exclamation from './img/svg/exclamation';
+import RightArrow from './img/svg/right-arrow'
+
+export {
+    Development,
+    Exclamation,
+    RightArrow
+}
