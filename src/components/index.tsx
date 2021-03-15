2bf25149a8b4925224ef702638aaf675eeb6dda2
Nathan Scott <contact@nathanscott.net>
2021-03-15 14:53:18 +0000

    added components, modified social links

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+import { SocialLinks } from './social-links/index';
+import { Header } from './header/index';
+import { TechnicalKnowledge } from './technical-knowledge/index';
+
+export {
+    Header,
+    SocialLinks,
+    TechnicalKnowledge
+}
---
+++ src/components/social-links/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Instagram, Github, Email } from '../img';
+
+interface ISocialLinks {
+    fill?: string;
+}
+export const SocialLinks = ({ fill }: ISocialLinks) => {
+    return (
+        <SocialLinksContainer>
+            <SocialLinksTag
+                target="_blank"
+                href="https://www.instagram.com/nathanscott285"
+            >
+            </SocialLinksTag>
+            <SocialLinksTag
+                target="_blank"
+                href="https://github.com/NathanScott85"
+            >
+                <Github className="social-links-svg" fill={fill} />
+            </SocialLinksTag>
+            <SocialLinksTag href="mailto:contact@nathanscott.net">
+                <Email className="social-links-svg" fill={fill} />
+            </SocialLinksTag>
+        </SocialLinksContainer>
+    );
+};
+
+const SocialLinksContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    max-width: 300px;
+    .social-links-svg:hover {
+        padding-bottom: 3px;
+    }
+`;
+
+const SocialLinksTag = styled.a`
+    margin: 5px 5px 0px 0px;
+`;
