b6bb48b1a9f42f2e8af463ef5382f3df1b67e450
Nathan Scott <contact@nathanscott.net>
2021-03-15 14:50:17 +0000

    added personal image, modified email, added lib

---
+++ src/lib/website-content.ts
@@ -0,0 +1,15 @@
+export const vitae = [
+    {
+        title: 'Frontend:',
+        content: 'HTML, CSS, Styled Components, Javascript, Typescript',
+    },
+    {
+        title: 'Frameworks:',
+        content:
+            'React, Bootstrap, Express.js, Nest.js,  Apollo-Server(GraphQL)',
+    },
+    {
+        title: 'Design:',
+        content: 'Figma',
+    }
+];
