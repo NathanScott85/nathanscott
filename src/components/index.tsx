d87858a49353b614ebb9a52a9ae237f77d9aa7a2
Nathan Scott <contact@nathanscott.net>
2021-03-17 15:44:24 +0000

    added contact form

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import { SocialLinks } from './social-links/index';
 import { Header } from './header/index';
 import { TechnicalKnowledge } from './technical-knowledge/index';
+import { ContactForm } from './contact-form';
 
-export { Header, SocialLinks, TechnicalKnowledge };
+export { ContactForm, Header, SocialLinks, TechnicalKnowledge };
